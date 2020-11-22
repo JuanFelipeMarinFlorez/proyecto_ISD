fb9aab50e4a96ef2492d210551eecfeb2039d6d1
Juan Felipe Marin <juanfe9964@gmail.com>
2020-11-21 23:40:56 -0500

    Se crearon los generadores auto. de vacunas y peticiones de vacunas

---
+++ Distribuidos2/src/eps/GeneradorSolicitudes.java
@@ -0,0 +1,45 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import  java.util.Random; 
+
+public class GeneradorSolicitudes extends Thread{
+
+    public GeneradorSolicitudes(){
+
+    }
+
+    public void run(){
+        try{
+            while(true){
+                try {
+                    File archivo = new File("Vacunas.txt");
+                    FileWriter escribir = new FileWriter(archivo, true);
+                    int valorEntero = (int)Math.floor(Math.random()*(10-1+1)+1);
+                    for (int i = 0; i <  valorEntero; i++) {
+                        escribir.write("Vacuna1");
+                        escribir.write("\r\n"); 
+                    }
+                    valorEntero = (int)Math.floor(Math.random()*(10-1+1)+1);
+                    for (int i = 0; i <  valorEntero; i++) {
+                        escribir.write("Vacuna2");
+                        escribir.write("\r\n"); 
+                    }
+                    valorEntero = (int)Math.floor(Math.random()*(10-1+1)+1);
+                    for (int i = 0; i <  valorEntero; i++) {
+                        escribir.write("Vacuna3");
+                        escribir.write("\r\n"); 
+                    }
+                    escribir.close();
+                } 
+                catch (Exception e) {
+                    System.out.println("Error al generar solicitudes de vacunas");
+                }
+            Thread.sleep(10000);
+            }
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Distribuidos2/src/ips/GenradorVacunas.java
@@ -0,0 +1,45 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import  java.util.Random; 
+
+public class GeneradorVacunas extends Thread{
+
+    public GeneradorVacunas(){
+
+    }
+
+    public void run(){
+        try{
+            while(true){
+                try {
+                    File archivo = new File("Vacunas.txt");
+                    FileWriter escribir = new FileWriter(archivo, true);
+                    int valorEntero = (int)Math.floor(Math.random()*(10-1+1)+1);
+                    for (int i = 0; i <  valorEntero; i++) {
+                        escribir.write("Vacuna1");
+                        escribir.write("\r\n"); 
+                    }
+                    valorEntero = (int)Math.floor(Math.random()*(10-1+1)+1);
+                    for (int i = 0; i <  valorEntero; i++) {
+                        escribir.write("Vacuna2");
+                        escribir.write("\r\n"); 
+                    }
+                    valorEntero = (int)Math.floor(Math.random()*(10-1+1)+1);
+                    for (int i = 0; i <  valorEntero; i++) {
+                        escribir.write("Vacuna3");
+                        escribir.write("\r\n"); 
+                    }
+                    escribir.close();
+                } 
+                catch (Exception e) {
+                    System.out.println("Error al crear las vacunas");
+                }
+            Thread.sleep(10000);
+            }
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
